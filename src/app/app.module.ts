2f655d3beb8484d773ad77cf4ac61c2b87ec6df8
holic5839 <holic5839@gmail.com>
2018-08-07 15:09:19 +0900

    sign up success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NotfoundComponent } from './notfound.component';
@@ -52,7 +54,7 @@ const routes: Routes = [
     YapenContainerComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule, HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
